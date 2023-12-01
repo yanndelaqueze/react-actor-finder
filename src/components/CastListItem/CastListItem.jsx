102b30ce88c7c5f6bdffee4ae8424cffc92d1a2e
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-12-01 12:51:33 +0100

    no photo

--- src/components/CastListItem/CastListItem.jsx
+++ src/components/CastListItem/CastListItem.jsx
@@ -1,15 +1,19 @@
 import s from "./style.module.css";
 import { SMALL_IMAGE_BASE_URL } from "../../config";
+import no_photo from "../../assets/images/no-photo.png";
 
 export function CastListItem({ castMember }) {
+  function getPhoto() {
+    if (castMember.profile_path) {
+      return SMALL_IMAGE_BASE_URL + castMember.profile_path;
+    } else {
+      return no_photo;
+    }
+  }
   return (
     <div>
       <div className={s.container}>
-        <img
-          className={s.img}
-          src={SMALL_IMAGE_BASE_URL + castMember.profile_path}
-          alt={castMember.name}
-        />
+        <img className={s.img} src={getPhoto()} alt={castMember.name} />
         <div className={s.name}>{castMember.name}</div>
         <div className={s.character}>as {castMember.character}</div>
       </div>
--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -7,7 +7,9 @@ export function MovieDetail({ record }) {
         <div className={s.title}>{record.original_title}</div>
       </div>
       <div className={s.release_date}>
-        ({record.release_date.substring(0, 4)})
+        ({record.release_date.substring(0, 4)}
+        {" - "}
+        {record.production_countries[0].name})
       </div>
       <div className={s.overview}>{record.overview}</div>
     </div>
